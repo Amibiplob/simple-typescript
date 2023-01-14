5447937fbdba111344efb8732359b68861bbb71b
Amibiplob <muhammadbiplob7@gmail.com>
2023-01-14 18:46:03 +0600

    Final Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "./Button";
+import Card from "./Card";
+import Footer from "./Footer";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col gap-10 m-20">
+      <Home></Home>
+      <h1 className="text-6xl leading-relaxed">
+        This is a simple react page. This is my first React Typescript with
+        Tailwind CSS and Daisy UI
+      </h1>
+      <Button></Button>
+      <Card></Card>
+      <Footer></Footer>
     </div>
   );
 }
